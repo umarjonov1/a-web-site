243c9ccb3f3a1357256a5c6f8f345a98815a5b03
umarjonov1 <islomumarjonov08@gmail.com>
2024-06-11 18:19:25 +0300

    somethingnew

--- project/views.py
+++ project/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
-
-HTML_STRING = '''
-<h1>Hello World</h1>
-'''
+from random import randint
+from articles.models import Article
+from django.template.loader import render_to_string
 
 def home_view(request):
+    obj = Article.objects.get(id=randint(1, 3))
+    context = {'object': obj,
+               'title': obj.title,
+               'id': obj.id,
+               'content': obj.content
+               }
+    HTML_STRING = render_to_string('home_view.html', context=context)
     return HttpResponse(HTML_STRING)
